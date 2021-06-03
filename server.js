c2443c3537073aec8b81825b1f278f9f69d5b6d5
Airat <khasanofficial14@gmail.com>
2021-06-03 14:04:15 +0300

    add dockerfile

--- server.js
+++ server.js
@@ -13,10 +13,9 @@ var corsOptions = {
 
 app.use(cors(corsOptions))
 
-// parse requests of content-type - application/json
+app.use()
 app.use(bodyParser.json())
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // simple route
@@ -25,7 +24,7 @@ app.get('/', (req, res) => {
 })
 require('./app/routes/auth.routes')(app)
 require('./app/routes/user.routes')(app)
-// set port, listen for requests
+
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
